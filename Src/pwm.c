4d00625a1e27e6620b3d2d1755fd4ee2cff106a8
hsrokon <hsrokonofficial@gmail.com>
2026-07-10 13:04:29 +0600

    calculating the timer values with PSC & ARR

--- Src/pwm.c
+++ Src/pwm.c
@@ -15,4 +15,12 @@ void main () {
 				  (0xB<<12); /* (0xB<<0)|(0xB<<4)|(0xB<<8)|(0xB<<12) means 0xBBBB which
 				  	  	  	  	  represents 0000 0000 0000 0000 1011 1011 1011 1011*/
 
+	//Setting up timer 2
+	TIM2->PSC = 71; /*Hardware always add a 1 to the value we write here, so write 71, we need result 72
+					For ease in calc, we want a round number for a clock to count,
+					here the clock will count 1M times/second (72MHz/72). */
+	TIM2->ARR = 2500; /*this defines after how many cycles/count we want the timer to reset,
+						since we want the clock to reset after 400count(400Hz),
+						so we want timer to reset after 2500 counts [My comment could be wrong in understanding]*/
+
 }
