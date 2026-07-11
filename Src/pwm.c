ae1d0a7599a9848c712e0c072b2e17807db556a6
hsrokon <hsrokonofficial@gmail.com>
2026-07-11 13:05:17 +0600

    fixed comments and cleard understanding

--- Src/pwm.c
+++ Src/pwm.c
@@ -2,8 +2,8 @@
 
 void main () {
 
-	RCC->APB1ENR |= (1<<0); //Timer 2 enabled
-	RCC->APB2ENR |= (1<<2); //GPIOA enabled
+	RCC->APB1ENR |= (1<<0); //Timer 2 clock peripheral enabled
+	RCC->APB2ENR |= (1<<2); //GPIOA clock peripheral enabled
 
 	//Resetting pin 0,1,2,3
 	GPIOA->CRL &= ~(0xFFFF); // 0xFFFF means 0000 0000 0000 0000 1111 1111 1111 1111
@@ -19,8 +19,10 @@ void main () {
 	TIM2->PSC = 71; /*Hardware always add a 1 to the value we write here, so write 71, we need result 72
 					For ease in calc, we want a round number for a clock to count,
 					here the clock will count 1M times/second (72MHz/72). */
-	TIM2->ARR = 2500; /*this defines after how many cycles/count we want the timer to reset,
+	TIM2->ARR = 2499; /*this defines after how many cycles/count we want the timer to reset,
 						since we want the clock to reset after 400count(400Hz),
-						so we want timer to reset after 2500 counts [My comment could be wrong in understanding]*/
+						so after each 2500 cycles the timer will reset
+						>> 0 - 2499 gives you 2500 total steps(1MHz/2500 = 400Hz)
+						>> 1,000,000 ticks/sec divided by 2500 steps = exactly 400Hz PWM cycle rate*/
 
 }
